ef5e6b13d64ffcba989d10f522e6b2c75e84bee0
Teshome-worku <teshomeworku96@gmail.com>
2026-02-10 13:35:09 +0300

    order receive logic added

--- index.js
+++ index.js
@@ -1,12 +1,60 @@
-const express = require('express');
+// const express = require('express');
+// const app = express();
+// app.use(express.json());
+// const orderRoutes = require('./routes/order');
+// app.use("/order", orderRoutes);
+// app.get("/", (req, res) => {
+//     res.send("Welcome to Food Order App Backend");
+// })
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on http://localhost:${PORT}`);
+// })
+
+const express = require("express");
+const cors = require('cors');
+
 const app = express();
+
+app.use(cors());
 app.use(express.json());
-const orderRoutes = require('./routes/order');
-app.use("/order", orderRoutes);
-app.get("/", (req, res) => {
-    res.send("Welcome to Food Order App Backend");
+
+// TEMP: mock orders storage
+const orders = [
+
+];
+
+app.get("/api/orders", (req, res) => {
+    // res.send(orders)
+    res.status(200).json({
+        message: "available orders",
+        order: orders
+
+    })
 })
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-})
+app.post("/api/orders", (req, res) => {
+    const order = req.body;
+
+    if (!order || !order.items || order.items.length === 0) {
+        return res.status(400).json({ message: "Invalid order" });
+    }
+
+    const savedOrder = {
+        id: Date.now(),
+        ...order,
+        status: "pending",
+    };
+
+    orders.push(savedOrder);
+
+    console.log("New order received:", savedOrder);
+
+    res.status(201).json({
+        message: "Order created",
+        orderId: savedOrder.id,
+    });
+});
+
+app.listen(5000, () => {
+    console.log("Server running on http://localhost:5000");
+});
